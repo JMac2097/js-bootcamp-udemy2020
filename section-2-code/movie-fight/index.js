ecb11f68b082ffb1fd6622b3e021dfb44f235669
John McKie <john.mckie@remarkable.net>
2020-04-27 16:16:19 +0100

    some late changes

---
+++ section-2-code/movie-fight/index.js
@@ -0,0 +1,24 @@
+
+const fetchData = async (searchTerm) => {
+    const response = await axios.get('http://www.omdbapi.com/', {
+        params: {
+            apikey: 'e987aca7',
+            s: searchTerm
+        }
+    });
+
+    console.log(response.data);
+};
+
+let timeoutId;
+const input = document.querySelector('input');
+const onInput = event => {
+    if (timeoutId) {
+        clearTimeout(timeoutId);
+    }
+    timeoutId = setTimeout(() => {
+        fetchData(event.target.value);
+    }, 500);
+};
+
+input.addEventListener('input', onInput);
